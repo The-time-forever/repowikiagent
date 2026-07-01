1e33eef9262ec9cb4e3cdedd8af598b650a176bb
The-time-forever <2254298277@qq.com>
2026-07-01 08:55:41 -0700

    chore: align cli version with 0.2.0

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
     .name('repowiki')
     .description('RepoWiki: 本地化代码库 Wiki 知识库生成工具')
-    .version('0.1.0');
+    .version('0.2.0');
 
 // 注册子命令
 program.addCommand(generateCommand);
